3994c5a61c4a0f04312471f75c49fc2a8a386f03
ooeunz <yuns994@gmail.com>
2019-11-15 17:12:36 +0900

    Create project and add dependencies

---
+++ src/main/java/com/chatbot/mark1/Mark1Application.java
@@ -0,0 +1,14 @@
+package com.chatbot.mark1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mark1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mark1Application.class, args);
+        System.out.println("Start Mark-1 chatbot service!");
+    }
+
+}
